39b3b48153b8000c5d5f80254cfbe5e59f97d208
lukas <lukas.schuwo@gmail.com>
2023-07-18 13:35:33 +1000

    Updated mission modifiers

--- mission_manager.py
+++ mission_manager.py
@@ -16,20 +16,24 @@ modifiers_dict['Low-Intensity Hunting Grounds'] = {'short': 'Low-Int Hunting Gro
 modifiers_dict['Hunting Grounds'] = {'short': 'Hunting Grounds', 'emoji': ':dog2:', 'tough': False}
 modifiers_dict['Low-Intensity Gauntlet (Power Supply Interruption)'] = {'short': 'Low-Int Power Supply', 'tough': False}
 modifiers_dict['Low-Intensity Engagement Zone'] = {'short': 'Low-Int', 'tough': False}
-modifiers_dict['Power Supply Interruption'] = {'short': 'Power Supply', 'tough': False}
+modifiers_dict['Power Supply Interruption'] = {'short': 'Power Supply Interruption', 'tough': False}
+modifiers_dict['Hunting Grounds (Power Supply Interruption)'] = {'short': 'Hunting Grounds Power Supply Interruption', 'tough': False}
 # Tough:
 modifiers_dict['Hi-Intensity Engagement Zone'] = {'short': 'Hi-Intensity', 'emoji': '', 'tough': True}
-modifiers_dict['Hi-Intensity Shock Troop Gauntlet'] = {'short': 'Hi-Int Shock Troop Gauntlet',
-                                                       'emoji': ':boxing_glove:', 'tough': True}
+modifiers_dict['Hi-Intensity Shock Troop Gauntlet'] = {'short': 'Hi-Int Shock Troop Gauntlet', 'emoji': ':boxing_glove:', 'tough': True}
 modifiers_dict['Hi-Intensity Gauntlet (Hunting Grounds)'] = {'short': 'Hi-Int Hunting Grounds', 'emoji': ':dog2:',
                                                              'tough': True}
-modifiers_dict['Hi-Intensity Gauntlet (Power Supply Outage)'] = {'short': 'Hi-Int Power Supply',
-                                                                 'emoji': ':flashlight:', 'tough': True}
+modifiers_dict['Hi-Intensity Gauntlet (Power Supply Interruption)'] = {'short': 'Hi-Int Power Supply Interruption',
+                                                                 'emoji': ':new_moon:', 'tough': True}
 modifiers_dict['High challenge low intensity'] = {'short': 'Elite Resistance', 'emoji': ':woman_lifting_weight:',
                                                   'tough': True}
 modifiers_dict['Shock Troop Gauntlet'] = {'short': 'Shock Troop Gauntlet', 'emoji': ':gloves:', 'tough': True}
 
+
 side_missions_dict = {'side_mission_grimoire': ' :blue_book: Grimoires ', 'side_mission_tome': ' :scroll: Scriptures '}
+#work in progress:
+# emoji_dict = {'hi-intensity':':small-red-triangle', 'hunting grounds':'dog2', 'shock troop gauntlet':'gloves',
+#              'power supply':'new_moon', 'high challenge':'woman_lifting_weight', 'elite resistance':'woman_lifting_weight'}
 
 
 # Format an individual mission for a discord message. Includes map-id, smart timestamps and emojis for some modifiers
@@ -39,7 +43,7 @@ def format_mission_for_discord(mission_data):
     circ_name = modifiers_dict.get(circ_name, {}).get('short', circ_name)
     map_name = mission_data.get('name')
     sidemission = side_missions_dict.get(mission_data.get('sideMission'), '')
-    return f"<t:{int(mission_data['start'])}:R>  {emoji}**{circ_name}**  {map_name} {sidemission} `/mmtimport {mission_data['id']}`"
+    return f"<t:{int(mission_data['start'])}:R>  {emoji} **{circ_name}**  {map_name} {sidemission} `/mmtimport {mission_data['id']}`"
 
 
 # Return missions that started in the last 30 minutes
